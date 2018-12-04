6d49574b6ee133c11fcafb34c071734a85c29d6d
Tiffany Moral <tiffany.moral@gmail.com>
2018-12-04 01:56:59 -0500

    made a un-useful comment as a test

--- app/src/androidTest/java/com/example/jake/androidchess/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/jake/androidchess/ExampleInstrumentedTest.java
@@ -20,7 +20,7 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
+        //yer that's whatsup
         assertEquals("com.example.jake.androidchess", appContext.getPackageName());
     }
 }
