0041969311d9dcbb102a5c6bcaf9bfba6851e61d
Jake Van Osten <jakevanosten@gmail.com>
2018-12-06 00:51:18 -0500

    clickable cells

--- app/src/main/java/com/example/jake/androidchess/GameActivity.java
+++ app/src/main/java/com/example/jake/androidchess/GameActivity.java
@@ -10,6 +10,10 @@ import android.widget.ImageView;
 
 public class GameActivity extends AppCompatActivity {
 
+    boolean firstClickFlag = false;
+    ImageView space1,space2;
+    int firstX,firstY = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +22,15 @@ public class GameActivity extends AppCompatActivity {
 
     public void move(View v){
 
+        if(!firstClickFlag){ //first time clicking a space, store the imageview info for next click
+            space1 = (ImageView) v;
+            int[] locations = new int[2];
+            v.getLocationOnScreen(locations);
+            firstX = locations[0];
+            firstY = locations[1];
+            System.out.println("(" + firstX + "," + firstY + ")");
+        }
     }
+
 
 }
