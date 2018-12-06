f247f116e29b2f0970c88187745ecea7fa5749c5
Jake Van Osten <jakevanosten@gmail.com>
2018-12-05 19:38:06 -0500

    Created title page for app, buttons not functional yet

--- app/src/androidTest/java/com/example/jake/androidchess/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/jake/androidchess/ExampleInstrumentedTest.java
@@ -20,8 +20,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-        //yer that's whatsup
-        // sketit
         assertEquals("com.example.jake.androidchess", appContext.getPackageName());
     }
 }
