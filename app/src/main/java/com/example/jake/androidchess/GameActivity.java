d9267dbd32dfe1e33643bc68f424df4dcfb4027b
Jake Van Osten <jakevanosten@gmail.com>
2018-12-06 01:59:08 -0500

    Halfway done locating coordinates of clicked pieces

--- app/src/main/java/com/example/jake/androidchess/GameActivity.java
+++ app/src/main/java/com/example/jake/androidchess/GameActivity.java
@@ -12,7 +12,8 @@ public class GameActivity extends AppCompatActivity {
 
     boolean firstClickFlag = false;
     ImageView space1,space2;
-    int firstX,firstY = 0;
+    android.support.v7.widget.GridLayout grid;
+    int row,col = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +25,29 @@ public class GameActivity extends AppCompatActivity {
 
         if(!firstClickFlag){ //first time clicking a space, store the imageview info for next click
             space1 = (ImageView) v;
-            int[] locations = new int[2];
-            v.getLocationOnScreen(locations);
-            firstX = locations[0];
-            firstY = locations[1];
-            System.out.println("(" + firstX + "," + firstY + ")");
+            String pieceID = getResources().getResourceName(v.getId());
+            int[] locations = getPieceIndex(v, pieceID);
+            row = locations[0];
+            col = locations[1];
+            System.out.println("(" + row + "," + col + ")");
         }
+    }
+
+    public int[] getPieceIndex(View v, String id){
+        grid = findViewById(R.id.board);
+        int[] location = new int[2];
+        int childCount = grid.getChildCount();
+        for(int i=0;i<childCount;i++){
+            if(id == getResources().getResourceName((grid.getChildAt(i)).getId())){ //matches ID
+                location[0] = i/8;
+                location[1] = i%8;
+                break;
+            }else{
+                location[0] = -1;
+                location[1] = -1;
+            }
+        }
+        return location;
     }
 
 
