76de71e8adfa39c6c42e1bed7fc01646f73c17bb
Tiffany Moral <tiffany.moral@gmail.com>
2018-12-06 00:18:33 -0500

    minor change, doesn't do much

--- app/src/main/java/com/example/jake/androidchess/GameActivity.java
+++ app/src/main/java/com/example/jake/androidchess/GameActivity.java
@@ -1,13 +1,21 @@
 package com.example.jake.androidchess;
 
+import android.support.annotation.XmlRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+
 
 public class GameActivity extends AppCompatActivity {
+    public ImageView bp1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
     }
+
+
 }
+
