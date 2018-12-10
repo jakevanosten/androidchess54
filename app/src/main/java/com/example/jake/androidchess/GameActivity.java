99cb594ac81a1c69cecd7d4ad4d40545c80b8cf8
Tiffany Moral <tiffany.moral@gmail.com>
2018-12-10 01:53:41 -0500

    getting ready to convert this jawn

--- app/src/main/java/com/example/jake/androidchess/GameActivity.java
+++ app/src/main/java/com/example/jake/androidchess/GameActivity.java
@@ -8,16 +8,44 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.GridLayout;
 import android.widget.ImageView;
+import android.widget.TextView;
+import com.example.gameBoard.Board;
+import com.example.gamePiece.Bishop;
+import com.example.gamePiece.King;
+import com.example.gamePiece.Queen;
+import com.example.gamePiece.Pawn;
+import com.example.gamePiece.Rook;
+import com.example.gamePiece.Knight;
+import com.example.gamePiece.BlankSpace;
+import com.example.gamePiece.CellType;
+import com.example.gamePiece.Legend;
 
 
 public class GameActivity extends AppCompatActivity {
 
+    //declare bishop pieces
+    Bishop bB1;
+    Bishop bB2;
+    Bishop wB1;
+    Bishop wB2;
+
+    //declare knight pieces
+    Knight bK1;
+    Knight bK2;
+    Knight wK1;
+    Knight wK2;
+
+    //declare king pieces
+
+
+    public void setDisplayBoard(TextView[][] displayBoard) {
+        DisplayBoard = displayBoard;
+    }
+
     boolean firstClickFlag = true;
-    View space1,space2;
-    android.widget.GridLayout grid;
-    int firstLoc;
-    int space1Color,space2Color;
-    View[] pieceOrder;
+
+    public TextView[][] DisplayBoard = new TextView[8][8];
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,54 +56,9 @@ public class GameActivity extends AppCompatActivity {
 
     public void move(View v){
 
-
-        if(firstClickFlag){ //first time clicking a space, store the imageview info for next click
-            space1 = v;
-            space1Color = ((ColorDrawable) v.getBackground()).getColor();
-            firstLoc = getPieceIndex(v);
-            v.setBackgroundColor(Color.CYAN);
-            firstClickFlag = false;
-
-        }else{ //second space clicked, time to do the move
-
-            grid = findViewById(R.id.board);
-            for(int i=0;i<64;i++){
-                pieceOrder[i] = grid.getChildAt(i);
-            }
-
-
-            space2 = v;
-            space2Color = ((ColorDrawable) v.getBackground()).getColor();
-            int newLoc = getPieceIndex(v);
-
-            grid = findViewById(R.id.board);
-            space1.setBackgroundColor(space2Color);
-            space2.setBackgroundColor(space1Color);
-
-            View temp = space1;
-            pieceOrder[firstLoc] = space2;
-            pieceOrder[newLoc] = temp;
-
-            grid = new GridLayout(this);
-            for(int i=0;i<63;i++){
-                grid.addView(pieceOrder[i],i);
-            }
-
-            firstClickFlag = true;
-        }
     }
 
-    public int getPieceIndex(View v){
-        grid = findViewById(R.id.board);
-        int childCount = grid.getChildCount();
 
-        for(int i=0;i<childCount;i++) {
-            if (v.equals((View) grid.getChildAt(i))) {
-                return i;
-            }
-        }
-        return -1;
-    }
 
 
 }
