6b5e827e8cc2690a78450f4fbf624cefa63bc9af
Jake Van Osten <jakevanosten@gmail.com>
2018-12-10 12:21:14 -0500

    Now running, needed to merge repo again

--- app/src/main/java/com/example/jake/androidchess/GameActivity.java
+++ app/src/main/java/com/example/jake/androidchess/GameActivity.java
@@ -13,11 +13,6 @@ import android.widget.ImageView;
 public class GameActivity extends AppCompatActivity {
 
     boolean firstClickFlag = true;
-    View space1,space2;
-    android.widget.GridLayout grid;
-    int firstLoc;
-    int space1Color,space2Color;
-    View[] pieceOrder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,53 +24,10 @@ public class GameActivity extends AppCompatActivity {
     public void move(View v){
 
 
-        if(firstClickFlag){ //first time clicking a space, store the imageview info for next click
-            space1 = v;
-            space1Color = ((ColorDrawable) v.getBackground()).getColor();
-            firstLoc = getPieceIndex(v);
-            v.setBackgroundColor(Color.CYAN);
-            firstClickFlag = false;
-
-        }else{ //second space clicked, time to do the move
-
-            grid = findViewById(R.id.board);
-            for(int i=0;i<64;i++){
-                pieceOrder[i] = grid.getChildAt(i);
-            }
-
-
-            space2 = v;
-            space2Color = ((ColorDrawable) v.getBackground()).getColor();
-            int newLoc = getPieceIndex(v);
-
-            grid = findViewById(R.id.board);
-            space1.setBackgroundColor(space2Color);
-            space2.setBackgroundColor(space1Color);
-
-            View temp = space1;
-            pieceOrder[firstLoc] = space2;
-            pieceOrder[newLoc] = temp;
-
-            grid = new GridLayout(this);
-            for(int i=0;i<63;i++){
-                grid.addView(pieceOrder[i],i);
-            }
-
-            firstClickFlag = true;
-        }
     }
 
-    public int getPieceIndex(View v){
-        grid = findViewById(R.id.board);
-        int childCount = grid.getChildCount();
+    public void undo(View v){
 
-        for(int i=0;i<childCount;i++) {
-            if (v.equals((View) grid.getChildAt(i))) {
-                return i;
-            }
-        }
-        return -1;
     }
-
 
 }
